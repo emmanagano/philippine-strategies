24a83123381c503aeef26da53c11fe89cede316e
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2025-04-18 13:29:59 +0800

    google integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css"; // Ensure styles are loaded
 import Layout from "./components/Layout";
 
@@ -17,6 +18,9 @@ export const metadata: Metadata = {
   title: "Philippine Strategies",
   description:
     "Before charging in with the cavalry, one must always have a good scouting team",
+  verification: {
+    google: "EQqhGxhaHmSfsa0YO7JPHF_HL3EFa2h--C74nSnRbaI",
+  },
 };
 
 export default function RootLayout({
@@ -26,9 +30,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="gtm-head" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TSZNKNFN');
+        `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSZNKNFN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Layout>
           {" "}
           {/* ✅ This ensures the Navbar & Footer are on all pages */}
